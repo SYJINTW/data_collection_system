83c6cf7fa99f7cbc823b1fd54b94db1a95afd927
SYJINTW <yuanjiun911@outlook.com>
2022-03-08 20:08:11 +0800

    add sth

--- airsim_client/my_code/coverage_cal.py
+++ airsim_client/my_code/coverage_cal.py
@@ -1,14 +1,9 @@
 import pandas as pd
 import numpy as np
 import math
-filename = 'bunny_mesh_merge'
+from scipy.spatial.transform import Rotation as R
 
-def cal_surface(v1, v2, v3):
-    vector1 = v2-v1
-    vector2 = v3-v1
-    n_vector = np.cross(vector1, vector2)
-    d = -np.dot(v1,n_vector)
-    return np.append(n_vector, d)
+filename = 'bunny_mesh_merge'
 
 def vertex_in_the_normal_side(vertex_datas, v1, v2, point):
     '''
@@ -26,8 +21,9 @@ def vertex_in_the_normal_side(vertex_datas, v1, v2, point):
     vertex_bin = vertex_datas[:,-1]
     vertex_bin = np.where(vertex_bin>=d,True,vertex_bin)
     vertex_bin = np.where(vertex_bin<d,False,vertex_bin)
-    print(vertex_bin)
-    print(np.count_nonzero(vertex_bin))
+    # print(vertex_bin.shape[0])
+    vertex_bin = np.append([False],vertex_bin)
+    # print(vertex_bin.shape[0])
     return vertex_bin
     
 
@@ -42,6 +38,8 @@ def rotate_all_the_point(x,y,z,yaw,pitch,roll,vertex_datas):
     # | cos(yaw)cos(pitch) -cos(yaw)sin(pitch)sin(roll)-sin(yaw)cos(roll) -cos(yaw)sin(pitch)cos(roll)+sin(yaw)sin(roll)|
     # | sin(yaw)cos(pitch) -sin(yaw)sin(pitch)sin(roll)+cos(yaw)cos(roll) -sin(yaw)sin(pitch)cos(roll)-cos(yaw)sin(roll)|
     # | sin(pitch)          cos(pitch)sin(roll)                            cos(pitch)sin(roll)|
+    
+    
     Rz_yaw = np.array([
         [np.cos(yaw), -np.sin(yaw), 0],
         [np.sin(yaw),  np.cos(yaw), 0],
@@ -66,7 +64,15 @@ def rotate_all_the_point(x,y,z,yaw,pitch,roll,vertex_datas):
     # print(vertex_datas_rotate.shape[1])
     return vertex_datas_rotate
 
-
+def find_point_in_triangle(vertex_datas, v1, v2, v3, viewpoint, point):
+    '''
+    vetex_data = [id, x, y, z]
+    v1 = [x,y,z]
+    v2 = [x,y,z]
+    v3 = [x,y,z]
+    point = [x, y, z]
+    '''
+        
 
 
 def main():
@@ -95,96 +101,72 @@ def main():
     viewpoint_c = np.array([1,1,-1.8])
     viewpoint_d = np.array([1,-1,-1.8])
     
+    # read file
+    # tri_data = [id,v1,v2,v3]
     tri_datas = pd.read_csv(f'./obj_source/{filename}_tri_datas.csv').to_numpy()
+    # vertex_data = [id,x,y,z]
     vertex_datas = pd.read_csv(f'./obj_source/{filename}_vertex_datas.csv').to_numpy()
 
+    # change all point into viewpoint cooridinate
     rotate_all_the_point(x,y,z,yaw,pitch,roll,vertex_datas)
 
     # create a binary list to store vertex usage
-    vertex_bin = np.ones(vertex_datas.shape[0])
+    vertex_bin = np.ones(vertex_datas.shape[0]+1)
+    tri_bin = np.ones(tri_datas.shape[0]+1)
+    vertex_bin[0] = 0
+    tri_bin[0] = 0
     
     # 1
     # delete outside(-)
-    vertex_bin = np.logical_and(vertex_in_the_normal_side(vertex_datas, viewpoint_a-viewpoint_center, viewpoint_b-viewpoint_center, viewport_pos),vertex_bin)
+    vertex_bin = np.logical_and(vertex_in_the_normal_side(vertex_datas, viewpoint_a, viewpoint_b, [0,0,0]), vertex_bin)
     print(vertex_bin)
     print(np.count_nonzero(vertex_bin))
-    vertex_bin = np.logical_and(vertex_in_the_normal_side(vertex_datas, viewpoint_b-viewpoint_center, viewpoint_c-viewpoint_center, viewport_pos),vertex_bin)
+    vertex_bin = np.logical_and(vertex_in_the_normal_side(vertex_datas, viewpoint_b, viewpoint_c, [0,0,0]), vertex_bin)
     print(vertex_bin)
     print(np.count_nonzero(vertex_bin))
-    vertex_bin = np.logical_and(vertex_in_the_normal_side(vertex_datas, viewpoint_c-viewpoint_center, viewpoint_d-viewpoint_center, viewport_pos),vertex_bin)
+    vertex_bin = np.logical_and(vertex_in_the_normal_side(vertex_datas, viewpoint_c, viewpoint_d, [0,0,0]), vertex_bin)
     print(vertex_bin)
     print(np.count_nonzero(vertex_bin))
-    vertex_bin = np.logical_and(vertex_in_the_normal_side(vertex_datas, viewpoint_d-viewpoint_center, viewpoint_a-viewpoint_center, viewport_pos),vertex_bin)
+    vertex_bin = np.logical_and(vertex_in_the_normal_side(vertex_datas, viewpoint_d, viewpoint_a, [0,0,0]), vertex_bin)
     print(vertex_bin)
     print(np.count_nonzero(vertex_bin))
-    tri_datas = del_face_from_vertex(tri_datas,vertex_bin)
+    
+    # 2
+    # change tri binary array
+    for tri_data in tri_datas:
+        for i in range(1,4):
+            if vertex_bin[tri_data[i]] == False:
+                tri_bin[tri_data[0]] = False
+    print("Numbers of triangle: ", tri_bin.shape[0])
+    print("Numbers of TRUE triangle: ", np.count_nonzero(tri_bin))
 
 
-    # # 2
-    # nor_check = np.array([
-    #                     [1,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    #                     [0,1,0,0,0,0,0,0,0,0,0,0,0,0],
-    #                     [0,0,1,0,0,0,0,0,0,0,0,0,0,0],
-    #                     [0,0,0,1,0,0,0,0,0,0,0,0,0,0],
-    #                     [0,0,0,0,1,0,0,0,0,0,0,0,0,0],
-    #                     [0,0,0,0,0,1,0,0,0,0,0,0,0,0],
-    #                     [0,0,0,0,0,0,1,0,0,0,0,0,0,0],
-    #                     [0,0,0,0,0,0,0,1,0,0,0,0,0,0],
-    #                     [0,0,0,0,0,0,0,0,1,0,0,0,0,0],
-    #                     [0,0,0,0,0,0,0,0,0,1,0,0,0,0],
-    #                     [0,0,0,0,0,0,0,0,0,0,1,0,0,0],
-    #                     [0,0,0,0,0,0,0,0,0,0,0,1,0,0],
-    #                     [0,0,0,0,0,0,0,0,0,0,0,0,1,0],
-    #                     [0,0,0,0,0,0,0,0,0,0,0,0,0,viewpoint_mid_vector[0]],
-    #                     [0,0,0,0,0,0,0,0,0,0,0,0,0,viewpoint_mid_vector[1]],
-    #                     [0,0,0,0,0,0,0,0,0,0,0,0,0,viewpoint_mid_vector[2]]])
+    
 
-    # tri_datas = tri_datas.dot(nor_check)
-    # tri_datas = tri_datas[tri_datas[:,-1]<=0]
+
+
 
     # # 3
-    # for idx in range(tri_datas.shape[0]-1,-1,-1):
-    #     a = np.array([tri_datas[idx][2],tri_datas[idx][3],tri_datas[idx][4]])
-    #     b = np.array([tri_datas[idx][6],tri_datas[idx][7],tri_datas[idx][8]])
-    #     c = np.array([tri_datas[idx][10],tri_datas[idx][11],tri_datas[idx][12]])
-        
-    #     surface1 = cal_surface(viewport_pos, a, b)
-    #     surface_check = [
-    #                 [1,0,0,0,0],
-    #                 [0,1,0,0,surface1[0]],
-    #                 [0,0,1,0,surface1[1]],
-    #                 [0,0,0,1,surface1[2]],
-    #                 ]
-    #     vertex_datas = vertex_datas[:,:-1]
-    #     vertex_datas = vertex_datas.dot(surface_check)
-    #     vertex_datas = vertex_datas[vertex_datas[:,-1]>surface1[3]]
+    # for tri_data in tri_datas:
+    #     if tri_bin[tri_data[0]] == True:
+    #         v1 = np.array(vertex_datas[tri_data[1]-1][1:])
+    #         v2 = np.array(vertex_datas[tri_data[2]-1][1:])
+    #         v3 = np.array(vertex_datas[tri_data[3]-1][1:])
+    #         vertex_bin = np.logical_and(vertex_in_the_normal_side(vertex_datas, v2, v1, [0,0,0]), vertex_bin)
+    #         vertex_bin = np.logical_and(vertex_in_the_normal_side(vertex_datas, v3, v2, [0,0,0]), vertex_bin)
+    #         vertex_bin = np.logical_and(vertex_in_the_normal_side(vertex_datas, v1, v3, [0,0,0]), vertex_bin)
 
-    #     surface2 = cal_surface(viewport_pos, b, c)
-    #     surface_check = [
-    #                 [1,0,0,0,0],
-    #                 [0,1,0,0,surface2[0]],
-    #                 [0,0,1,0,surface2[1]],
-    #                 [0,0,0,1,surface2[2]],
-    #                 ]
-    #     vertex_datas = vertex_datas[:,:-1]
-    #     vertex_datas = vertex_datas.dot(surface_check)
-    #     vertex_datas = vertex_datas[vertex_datas[:,-1]>surface1[3]]
+    # # 4
+    # # change tri binary array
+    # for tri_data in tri_datas:
+    #     for i in range(1,4):
+    #         if vertex_bin[tri_data[i]] == False:
+    #             tri_bin[tri_data[0]] = False
+    # print("Numbers of triangle: ", tri_bin.shape[0])
+    # print("Numbers of TRUE triangle: ", np.count_nonzero(tri_bin))
 
-    #     surface3 = cal_surface(viewport_pos, c, a)
-    #     surface_check = [
-    #                 [1,0,0,0,0],
-    #                 [0,1,0,0,surface3[0]],
-    #                 [0,0,1,0,surface3[1]],
-    #                 [0,0,0,1,surface3[2]],
-    #                 ]
-    #     vertex_datas = vertex_datas[:,:-1]
-    #     vertex_datas = vertex_datas.dot(surface_check)
-    #     vertex_datas = vertex_datas[vertex_datas[:,-1]>surface1[3]]
-        
-        
-
-
-
+    # 5
+    
 
     # df = pd.DataFrame(tri_datas)
     # df_v = pd.DataFrame(vertex_datas)
